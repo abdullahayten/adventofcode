c60015bc8f0ecbdc787b1d73d3eec22c4f351291
Abdullah AYTEN <abdullahayten93@gmail.com>
2022-01-27 20:58:49 +0300

    Add project files.

---
+++ adventofcode/Program.cs
@@ -0,0 +1,16 @@
+﻿using adventofcode._2015;
+using System;
+
+namespace adventofcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1 d1 = new Day1();
+            d1.ShowResult();
+
+            Console.ReadKey();
+        }
+    }
+}
